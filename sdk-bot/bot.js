4280fa87462c7e304d93ef3ef5f86cc41560cea0
JMeckel <jan.meckelholt@mercedes-benz.com>
2025-08-01 07:23:13 +0200

    modifiy welcome message

--- sdk-bot/bot.js
+++ sdk-bot/bot.js
@@ -14,7 +14,7 @@ class EchoBot extends ActivityHandler {
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Hello and welcome to EchoBot!';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
@@ -45,7 +45,7 @@ class QnABot extends ActivityHandler {
         });
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Hello and welcome to QnABot!';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
